299e7bf3326043972d0af2596a7c9e55c55d8b28
seon yeong hun <sprax@naver.com>
2022-10-13 09:29:24 +0900

    first commit
    first commit

---
+++ script/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    // 닫기 버튼 누르면 다시 나타나지 않게 변경한다.
+    $("#close_banner").click(function(){
+        $("#banner").addClass("d-none");
+    })
+
+    // 전체메뉴 누르면 모든 메뉴를 펼쳐놓는다.
+
+    // 캐러셀 기능을 커스터마이징한다.
+})
