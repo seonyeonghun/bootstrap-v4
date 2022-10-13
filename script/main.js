919eccc3fd9bf4ed549549f71b5b6e41dfdf74b6
seon yeong hun <sprax@naver.com>
2022-10-13 11:18:00 +0900

    update index 2/2
    script, button, badge 추가

--- script/main.js
+++ script/main.js
@@ -4,6 +4,20 @@ $(function(){
         $("#banner").addClass("d-none");
     })
 
+    // cart 버튼 누르면, 주문상품의 상품명, 수량, 가격을 업데이트한다.
+    // 그리고 나서 결제창으로 이동할것인지 확인한다.
+    $("#cart").click(function(){
+        var order = {
+            buyItem: "라움 2인 그릇 6P",
+            buyCount: 1,
+            buyPrice: 39500
+        }
+        // .prompt() : 사용자에게 입력창을 띄우는 내장객체 메소드 [오래된 메소드] ==> form을 사용해서 처리!
+        // .confirm() : 확인 true, 취소 false로 의사선택창을 띄움
+        var userChoice = confirm("장바구니에 담았습니다, 결제창으로 이동하시겠습니까?");
+        userChoice ? location.href="/payment" : null;
+    })
+
     // 전체메뉴 누르면 모든 메뉴를 펼쳐놓는다.
 
     // 캐러셀 기능을 커스터마이징한다.
